433b4f2c286da20b62c778bb8e3af3690d4440d0
林志斌 <linzb93@qq.com>
2024-06-20 10:59:37 +0800

    refactor:优化时间重叠判断，支持跨天的

--- packages/core/lib/timeOverlay/index.test.ts
+++ packages/core/lib/timeOverlay/index.test.ts
@@ -1,52 +1,38 @@
 import { describe, it, expect } from 'vitest';
-import { isTimeOverlay, validateTimeOverlay } from "./index";
+import { isTimeOverlay } from "./index";
 
 describe("判断时间是否重叠", () => {
-  function formatTimeList(list: string[]) {
-    return list.map(item => {
-      const seg = item.split('~');
-      return {
-        startTime: seg[0],
-        endTime: seg[1]
-      }
-    })
-  }
   it('两个时间段没有重叠', () => {
-    expect(isTimeOverlay(formatTimeList(['08:00~10:00', '13:00~15:00']))).toBeFalsy();
+    expect(isTimeOverlay('08:00~10:00', '13:00~15:00')).toBeFalsy();
   });
   it('两个时间段有交叉', () => {
-    expect(isTimeOverlay(formatTimeList(['08:00~10:00', '09:00~15:00']))).toBeTruthy();
+    expect(isTimeOverlay('08:00~10:00', '09:00~15:00')).toBeTruthy();
   });
   it('两个时间段相邻', () => {
-    expect(isTimeOverlay(formatTimeList(['08:00~10:00', '10:00~12:00']))).toBeFalsy();
+    expect(isTimeOverlay('08:00~10:00', '10:00~12:00')).toBeFalsy();
   });
   it('两个时间段包含', () => {
-    expect(isTimeOverlay(formatTimeList(['08:00~10:00', '09:00~09:30']))).toBeTruthy();
+    expect(isTimeOverlay('08:00~10:00', '09:00~09:30')).toBeTruthy();
   });
   it('两个时间段反包含', () => {
-    expect(isTimeOverlay(formatTimeList(['08:00~10:00', '06:00~12:30']))).toBeTruthy();
+    expect(isTimeOverlay('08:00~10:00', '06:00~12:30')).toBeTruthy();
   });
   it('两个时间段相等', () => {
-    expect(isTimeOverlay(formatTimeList(['08:00~10:00', '08:00~10:00']))).toBeTruthy();
+    expect(isTimeOverlay('08:00~10:00', '08:00~10:00')).toBeTruthy();
   });
-  it("时间组没有重叠", () => {
-    expect(
-      validateTimeOverlay(formatTimeList(["08:00~10:00", "12:00~15:00", "17:00~19:00"]))
-    ).toBeFalsy();
+  it("两个跨天的", () => {
+    expect(isTimeOverlay("08:00~02:00", "12:00~01:00")).toBeTruthy();
   });
-  it("时间组有交叉", () => {
-    expect(
-      validateTimeOverlay(formatTimeList(["08:00~10:00", "09:30~15:00", "17:00~19:00"]))
-    ).toBeTruthy();
+  it("第一个跨天的，第二个不跨天，有重叠", () => {
+    expect(isTimeOverlay("08:00~02:00", "00:30~01:00")).toBeTruthy();
   });
-  it("时间组有相邻", () => {
-    expect(
-      validateTimeOverlay(formatTimeList(["08:00~10:00", "10:00~15:00", "17:00~19:00"]))
-    ).toBeFalsy();
+  it("第一个跨天的，第二个不跨天，无重叠", () => {
+    expect(isTimeOverlay("08:00~02:00", "03:30~06:00")).toBeFalsy();
   });
-  it("时间组有包含", () => {
-    expect(
-      validateTimeOverlay(formatTimeList(["08:00~10:00", "09:30~10:00", "17:00~19:00"]))
-    ).toBeTruthy();
+  it("第一个不跨天，第二个跨天的，有重叠", () => {
+    expect(isTimeOverlay("08:00~10:00", "23:30~09:00")).toBeTruthy();
+  });
+  it("第一个不跨天，第二个跨天的，无重叠", () => {
+    expect(isTimeOverlay("08:00~10:00", "23:30~05:00")).toBeFalsy();
   });
 });
--- packages/core/lib/timeOverlay/index.ts
+++ packages/core/lib/timeOverlay/index.ts
@@ -19,36 +19,76 @@ function sortTime(timeList: TimeRange[]): TimeRange[] {
 }
 
 /**
+ * 判断时间范围是否是跨天的，
+ * @param {string} time - 时间范围，格式是HH:mm~HH:mm
+ * @returns {boolean} 是否跨天
+ * @example
+ * isCrossDay('10:00~04:10'); // true
+ * isCrossDay('13:00~21:05'); // false
+ */
+function isCrossDay(time:string) {
+  const [startTime, endTime] = time.split('~');
+  return timeIsSmaller(endTime, startTime);
+}
+/**
+* 将跨天的时间范围分割成两天的时间段
+* @param {string} period - 时间范围，格式是HH:mm~HH:mm
+* @returns {string[][]} - 分割后的时间范围
+* @example
+* splitPeriod('21:00~05:00'); // [['21:00','24:00'],['00:00','05:00']]
+* splitPeriod('02:00~05:00'); // [['02:00','05:00']]
+*/
+export function splitPeriod(period:string) {
+  let seg = [];
+  if (isCrossDay(period)) {
+      const [start, end] = period.split('~');
+      seg = [
+          [start, '24:00'],
+          ['00:00', end],
+      ];
+  } else {
+      seg = [period.split('~')];
+  }
+  return seg;
+}
+
+/**
  * 判断两个时间段是否重叠，如有重叠，返回true。
  * @param timeList: [{startTime, endTime}]，
  * @param time: 形如'22:00'的
  * @return {boolean} 是否存在重叠
  */
-export function isTimeOverlay(timeList: TimeRange[]): boolean {
-  if (!timejs(timeList[0].endTime).isAfter(timeList[1].startTime)) {
-    return false;
+/**
+ * 判断两个时间范围是否有重叠，支持跨天判断
+ * @param {*} range0 - 时间范围 ，格式是HH:mm~HH:mm
+ * @param {*} range1 - 时间范围 ，格式是HH:mm~HH:mm
+ * @returns {boolean}
+ * @example
+ * isTimeOverlay('03:00~07:00', '08:00~13:40'); // false
+ * isTimeOverlay('03:00~07:00', '08:00~03:40'); // true
+ */
+export function isTimeOverlay(range0:string, range1:string):boolean {
+  // 如果两个都是跨天的，就有重叠
+  if (isCrossDay(range0) && isCrossDay(range1)) {
+      return true;
   }
-  if (!timejs(timeList[1].endTime).isAfter(timeList[0].startTime)) {
-    return false;
+  if (isCrossDay(range0)) {
+      return splitPeriod(range0).some((time) => {
+          return isTimeOverlay(`${time[0]}~${time[1]}`, range1);
+      });
+  }
+  if (isCrossDay(range1)) {
+      return splitPeriod(range1).some((time) => {
+          return isTimeOverlay(`${time[0]}~${time[1]}`, range0);
+      });
+  }
+  const [startTime0, endTime0] = range0.split('~');
+  const [startTime1, endTime1] = range1.split('~');
+  if (!timeIsSmaller(startTime1, endTime0)) {
+      return false;
+  }
+  if (!timeIsSmaller(startTime0, endTime1)) {
+      return false;
   }
   return true;
-}
-
-/**
- * 判断一组时间段是否重叠，如有重叠，返回true。
- * @param timeList: [{startTime, endTime}]，
- * @param time: 形如'22:00'的
- * @return {boolean} 是否存在重叠
- */
-export function validateTimeOverlay(timeList: TimeRange[]): boolean {
-  for (let i = 0; i < timeList.length - 1; i++) {
-    for (let j = i + 1; j < timeList.length; j++) {
-      const target = [timeList[i], timeList[j]];
-      const timeCouple = sortTime(target);
-      if (isTimeOverlay(timeCouple)) {
-        return true;
-      }
-    }
-  }
-  return false;
 }
